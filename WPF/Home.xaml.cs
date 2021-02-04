cfbb28ab6863ddfc14bf9b47a0d41b38d5346b44
Dawid Kołcz <dawid.kolcz@microsoft.wsei.edu.pl>
2021-02-04 21:44:12 +0100

    Home page done.

---
+++ WPF/Home.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            Check check = new Check();
+            this.NavigationService.Navigate(check);
+        }
+        private void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void RemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
